03cbe5327626010f15c1b7d2d7301b4b44613403
fazerneo <160219263+fazerneo@users.noreply.github.com>
2024-06-15 00:53:22 +0800

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -1,13 +1,13 @@
-''' This exercise 3 was an interesting exercise. I initially built the nest if-elif-else inside an if-else
-but then I thought what if the user puts a float or str as an input. I tried and crash, then I researched
-and added the try-except to capture the value error but again it would crash if i tried a second value error.
+''' This exercise 3 was an interesting exercise. I initially built a three-dimensional nested if-elif-else
+but then I thought, what if the user puts a float or str as an input? I tried and crashed, then I researched
+and added the try-except to capture the value error, but again, it would crash if I tried a second value error.
 So then I added a while loop and added the code inside the try block so that as long as there is a 
-value error, the code will keep looping and asking for a valid integer. I initially tried using round function
-for floats issue but then thought that the user could add a string also as an input.'''
+value error, the code will keep looping and asking for a valid integer. I initially tried using the round function
+for the floats issue but then thought that the user could add a string also as an input.'''
 while True:
     try:
         num = int(input("enter a number between 1 and 7: "))
-        if num > 0 and num < 8:
+        if num in range(1,8):
             if num == 1:
                 print("Monday")
             elif num == 2:
