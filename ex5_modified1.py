ea276b951dfc058a6bdd5e4a71679ca4b3bd153c
fazerneo <160219263+fazerneo@users.noreply.github.com>
2024-06-15 00:58:25 +0800

    Update ex5_modified1.py

--- ex5_modified1.py
+++ ex5_modified1.py
@@ -1,15 +1,15 @@
-''' To modify this exercise, I used a while loop which will take a user input and if the user input is 
-end, then the loop will break. If it is not end then the input will be converted to an integer using the
-int function and the calculation will be done continuously as it is not supposed to break unless input is end.
-If there is value error then will print enter a valid value and continue the loop.'''
+''' To modify this exercise, I used a while loop, which takes user input, and if the user input is
+at the end, then the loop will break. If it is not "end," then the input will be converted to an integer using the
+int cast, and the calculation will be done continuously as it is not supposed to break unless the input is "end".
+If there is a value error, then will print, enter a valid value, and continue the loop.'''
 while True:
-    fahrenheit = input("enter temperature in fahrenheit (enter end to exit): ")
+    Fahrenheit = input("enter temperature in Fahrenheit (enter end to exit): ")
     
-    if fahrenheit == "end":
+    if Fahrenheit == "end":
         break
     
     try:
-        fahrenheit = int(fahrenheit)
-        print((fahrenheit - 32) * 5 / 9)
+        Fahrenheit = int(Fahrenheit)
+        print((Fahrenheit - 32) * 5 / 9)
     except ValueError:
         print("enter a valid value")
