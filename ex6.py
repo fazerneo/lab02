cce2dff923e26ebc51e5bd0e6949ba6f4243eefe
fazerneo <160219263+fazerneo@users.noreply.github.com>
2024-06-15 01:00:21 +0800

    Update ex6.py

--- ex6.py
+++ ex6.py
@@ -1,13 +1,13 @@
-# I firstly initialised a variable total with the value zero
+# I first initialized a variable total with the value of zero
 total = 0
 
-# Then I chose a for loop as we had to deal with a range of numbers that was finite. My range is inclusive of 23 and 579
+# Then, I chose a for loop as we had to deal with a finite range of numbers. My range is inclusive of 23 and 579
 for i in range(23, 579):
-    # I check whether it is an even number by using a modulo as an even number divided by 2 will have 0 as remainder
+    # I check whether it is an even number by using a modulo as an even number divided by 2 will have 0 as the remainder
     if i % 2 == 0:
-        # Then I add the even i to total and the new value is stored inside total.
+        # Then I add the even i to the total, and the new value is stored inside the total.
         # This loop continues until all even numbers in the range are used up.
         total = total + i
 
-# Then I print the total. My print statement is outside the for loop bcoz if it was inside then it would print the total in every iteration.
+# Then I print the total. My print statement is outside the for loop because if it were inside, it would print the total in every iteration.
 print(total)
