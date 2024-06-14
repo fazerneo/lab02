042791c41cece91bff290f080c47833038ca14b4
fazerneo <160219263+fazerneo@users.noreply.github.com>
2024-06-15 00:58:44 +0800

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -2,6 +2,6 @@
 
 #Taking integer inputs from users for the calculations. I made a for loop for 7 times as I wanted to make 7 calculations
 for i in range( 7):
-	fahrenheit = int(input("enter temperature in fahrenheit: "))
-#Printing the celsius value using the given formula
-	print((fahrenheit - 32) * 5 / 9)
+	Fahrenheit = int(input("enter temperature in Fahrenheit: "))
+#Printing the Celsius value using the given formula
+	print((Fahrenheit - 32) * 5 / 9)
